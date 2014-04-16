39ad0fcbc5246686903e13ca69d08c534291dd12
Jesse S <jelloeater@gmail.com>
2014-04-15 22:09:01 -0400

    Spits out JSON now YAY

--- src/main/webCommand.py
+++ src/main/webCommand.py
@@ -44,87 +44,83 @@ def webHandler(environ, start_response):
 	return retStr
 
 
-class dateTimeToJSON():
+class CurrentTime:  # Just a dummy class to hold stuff
 	def __init__(self):
-		self.rawJSONstr = self.dateTimeToJSON()
-
-
-	@staticmethod
-	def dateTimeToJSON():
 		import time
-		import json
+
 		# Example: "Mon Apr 14 23:20:23 2014"
-		rawJSON = ""
 
 		timeStr = time.asctime()
-		year = int(timeStr[20:24])
-		month = dateTimeToJSON.parseMonth(timeStr[4:7])
-		day = int(timeStr[8:10])
-		hour = int(timeStr[10:13])
-		minute = int(timeStr[14:16])
-		second = int(timeStr[17:19])
-		dayOfWeek = dateTimeToJSON.parseDay(timeStr[0:3])
 
-		# FIXME Need to add either class or dictionary to hold values for JSON converter
+		self.year = int(timeStr[20:24])
+		self.month = parseMonth(timeStr[4:7])
+		self.day = int(timeStr[8:10])
+		self.hour = int(timeStr[10:13])
+		self.minute = int(timeStr[14:16])
+		self.second = int(timeStr[17:19])
+		self.dayOfWeek = parseDay(timeStr[0:3])
 
-		rawJSON = json.JSONEncoder.encode()
-		# FIXME Move local vars into constructor and THEN have the constructor call the toJSON method
 
-		return rawJSON
+def dateTimeToJSON():
+	import json
+	rawJSON = ""
+	jsonBag = CurrentTime()
 
-	@staticmethod
-	def parseDay(dayStr):
-		if dayStr == "Sun": dayInt = 1
-		if dayStr == "Mon": dayInt = 2
-		if dayStr == "Tue": dayInt = 3
-		if dayStr == "Wed": dayInt = 4
-		if dayStr == "Thr": dayInt = 5
-		if dayStr == "Fri": dayInt = 6
-		if dayStr == "Sat": dayInt = 7
-		return dayInt
+	# FIXME Need to add either class or dictionary to hold values for JSON converter
+	x = json.dumps(jsonBag.__dict__)
+	print(x)
 
-	@staticmethod
-	def parseMonth(monthStr):
-		if monthStr == "Jan": monthInt = 1
-		if monthStr == "Feb": monthInt = 2
-		if monthStr == "Mar": monthInt = 3
-		if monthStr == "Apr": monthInt = 4
-		if monthStr == "May": monthInt = 5
-		if monthStr == "Jun": monthInt = 6
-		if monthStr == "Jul": monthInt = 7
-		if monthStr == "Aug": monthInt = 8
-		if monthStr == "Sep": monthInt = 9
-		if monthStr == "Oct": monthInt = 10
-		if monthStr == "Nov": monthInt = 11
-		if monthStr == "Dec": monthInt = 12
-		return monthInt
+	# FIXME Move local vars into constructor and THEN have the constructor call the toJSON method
+	return rawJSON
 
-	@staticmethod
-	def timeStringOutput(hour, minute):
-		minFormat = ""
 
-		# Convert to local time
-		hour += 5
-		amPm = "p"
+def parseDay(dayStr):
+	if dayStr == "Sun": dayInt = 1
+	if dayStr == "Mon": dayInt = 2
+	if dayStr == "Tue": dayInt = 3
+	if dayStr == "Wed": dayInt = 4
+	if dayStr == "Thr": dayInt = 5
+	if dayStr == "Fri": dayInt = 6
+	if dayStr == "Sat": dayInt = 7
+	return dayInt
 
-		if hour > 12: # This is twice in-case the time is over 24 hours, due to my bad math
-			hour -= 12
-			amPm = "a"
 
-		if hour > 12:
-			hour -= 12
+def parseMonth(monthStr):
+	if monthStr == "Jan": monthInt = 1
+	if monthStr == "Feb": monthInt = 2
+	if monthStr == "Mar": monthInt = 3
+	if monthStr == "Apr": monthInt = 4
+	if monthStr == "May": monthInt = 5
+	if monthStr == "Jun": monthInt = 6
+	if monthStr == "Jul": monthInt = 7
+	if monthStr == "Aug": monthInt = 8
+	if monthStr == "Sep": monthInt = 9
+	if monthStr == "Oct": monthInt = 10
+	if monthStr == "Nov": monthInt = 11
+	if monthStr == "Dec": monthInt = 12
+	return monthInt
+
+
+def timeStringOutput(hour, minute):
+	minFormat = ""
+	# Convert to local time
+	hour += 5
+	amPm = "p"
+	if hour > 12: # This is twice in-case the time is over 24 hours, due to my bad math
+		hour -= 12
 		amPm = "a"
+	if hour > 12:
+		hour -= 12
+	amPm = "a"
+	if minute < 9:
+		minFormat = "0"
 
-		if minute < 9:
-			minFormat = "0"
+	hour = str(hour)
+	minute = str(minute)
+	separator = ":"
+	retStr = hour + separator + minFormat + minute + amPm
+	return retStr
 
-		hour = str(hour)
-		minute = str(minute)
-		separator = ":"
-
-		retStr = hour + separator + minFormat + minute + amPm
-
-		return retStr
 
 if __name__ == "__main__": # Runs Script
 	# main() # TODO Re-enable main when done
